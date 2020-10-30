b9e74b395f45849b4f82de1961e66eb6b1b2c692
黎少康 <651900526@qq.com>
2020-10-30 16:26:01 +0800

    代码更新

--- src/main/java/com/suminfo/demo/utils/MarkdownUtils.java
+++ src/main/java/com/suminfo/demo/utils/MarkdownUtils.java
@@ -1,48 +1,78 @@
 package com.suminfo.demo.utils;
 
+import org.commonmark.Extension;
 import org.commonmark.ext.gfm.tables.TableBlock;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.ext.heading.anchor.HeadingAnchorExtension;
 import org.commonmark.node.Link;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.AttributeProvider;
+import org.commonmark.renderer.html.AttributeProviderContext;
+import org.commonmark.renderer.html.AttributeProviderFactory;
 import org.commonmark.renderer.html.HtmlRenderer;
 
-import java.util.Map;
+import java.util.*;
 
+/**
+ * MarkDown转HTML工具类
+ * @author xiaobai
+ * @date 2020/8/9 17:26
+ */
 public class MarkdownUtils {
+
     /**
-     * markdown格式转化html格式
+     * markdown格式转换成HTML格式
+     * @param markdown
+     * @return
      */
-    public static  String markdownToHtml(String markdown){
-        Parser parse = Parser.builder().build();
-        Node document =parse.parse(markdown);
+    public static String markdownToHtml(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
         HtmlRenderer renderer = HtmlRenderer.builder().build();
-
         return renderer.render(document);
-
     }
+
     /**
-     * 增加扩展(标题锚地，表格生成)
-     * H
+     * 增加扩展[标题锚点，表格生成]
+     * Markdown转换成HTML
+     * @param markdown
+     * @return
      */
-
-
+    public static String markdownToHtmlExtensions(String markdown) {
+        //h标题生成id
+        Set<Extension> headingAnchorExtensions = Collections.singleton(HeadingAnchorExtension.create());
+        //转换table的HTML
+        List<Extension> tableExtension = Arrays.asList(TablesExtension.create());
+        Parser parser = Parser.builder()
+                .extensions(tableExtension)
+                .build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder()
+                .extensions(headingAnchorExtensions)
+                .extensions(tableExtension)
+                .attributeProviderFactory(new AttributeProviderFactory() {
+                    public AttributeProvider create(AttributeProviderContext context) {
+                        return new CustomAttributeProvider();
+                    }
+                })
+                .build();
+        return renderer.render(document);
+    }
 
     /**
      * 处理标签的属性
      */
-    static  class CustomAttributeProvider implements AttributeProvider{
+    static class CustomAttributeProvider implements AttributeProvider {
         @Override
-        public void setAttributes(Node node, String s, Map<String, String> map) {
-            //改变a便签的target属性为_blank
-            if(node instanceof Link){
-                attributes.put("target","_blank");
+        public void setAttributes(Node node, String tagName, Map<String, String> attributes) {
+            //改变a标签的target属性为_blank
+            if (node instanceof Link) {
+                attributes.put("target", "_blank");
             }
-            if(node instanceof TableBlock){
-                attributes.put("class","ui celled table");
-
+            if (node instanceof TableBlock) {
+                attributes.put("class", "ui celled table");
             }
         }
     }
-
 }
